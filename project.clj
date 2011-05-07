888a1e2d793dc7c621751acaa95a0e78b5ba5d89
Paul Ingles <paul@oobaloo.co.uk>
2011-05-07 19:19:25 +0100

    add incanter and swank-clojure

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject machine-learning-in-action "1.0.0-SNAPSHOT"
   :description "Playing around converting the code samples from Machine Learning in Action to Clojure"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [incanter "1.2.3"]]
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]])
