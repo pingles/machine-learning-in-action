e95407ed6f4d45e1d292df678a65a86e619d7558
Paul Ingles <paul@oobaloo.co.uk>
2011-05-10 08:56:07 +0100

    another little refactor in euclidean-distance

--- src/machine_learning_in_action/ch2.clj
+++ src/machine_learning_in_action/ch2.clj
@@ -46,9 +46,9 @@
 (defn euclidean-distance
   "Calculates euclidean distance between vector xs and all vectors in matrix m."
   [xs m]
-  (let [xs-matrix (matrix (repeat (first (dim m)) xs))]
-    (sqrt (map sum-of-squares (minus xs-matrix
-                                     m)))))
+  (let [difference-matrix (minus (matrix (repeat (first (dim m)) xs))
+                                 m)]
+    (sqrt (map sum-of-squares difference-matrix))))
 
 (defn knn-classify
   "Classifies vector xs against nearest k neighbours in matrix m of known vectors labelled by labels"
